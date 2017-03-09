a1b0067173f90d087b2604f8bc3f1962eab3f2dd
rjchallis <rjchallis@gmail.com>
2017-03-09 12:49:21 +0000

    modify output filenames

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ fi
 cegma -g $ASSEMBLY
 
 if [ -s output.cegma.errors ]; then
-  mv  output.completeness_report /out/$ASSEMBLY.completeness_report.txt
+  mv  output.completeness_report /out/$ASSEMBLY.cegma.completeness_report.txt
   mv  output.cegma.gff /out/$ASSEMBLY.cegma.gff
 else
   mv  output.cegma.errors /out/$ASSEMBLY.cegma.errors
